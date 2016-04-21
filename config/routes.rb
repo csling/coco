613ac1349e9148d61a2d9e33793b445d2cfacf08
Christian Ling <christian.s.h.ling@gmail.com>
2016-04-20 22:21:23 -0700

    Install devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '', to: 'web_app#index'
   get 'hello_world', to: 'hello_world#index'
   # The priority is based upon order of creation: first created -> highest priority.
