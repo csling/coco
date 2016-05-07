e3c67b806f0112be953b437e81305fa1c8861eb2
Christian Ling <christian.s.h.ling@gmail.com>
2016-05-07 00:30:59 -0700

    Install redux-form library

---
+++ src/routes/ForEmployers/containers/ForEmployers.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {reduxForm} from 'redux-form';
+
+export const fields = ['contactName',
+  'companyName',
+  'contactEmailAddress',
+  'contactPhoneNumber'
+]
+
+const validate = (values) => {
+  const errors = {}
+  return errors
+}
+
+type Props = {
+  handleSubmit: Function,
+  fields: Object,
+}
+
+export class ForEmployers extends React.Component {
+  props: Props;
+
+  defaultProps = {
+    fields: {contactName, companyName, contactEmailAddress, contactPhoneNumber},
+  }
+
+  render() {
+    const { fields, handleSubmit } = this.props
+
+    return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input type="text" placeholder="Your Name" {...contactName}/>
+      </div>
+      <div>
+        <input type="text" placeholder="Company Name" {...companyName}/>
+      </div>
+      <div>
+        <input type="email" placeholder="Email Address" {...contactEmailAddress}/>
+      </div>
+      <div>
+        <input type="tel" placeholder="Phone #" {...contactEmailAddress}/>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+    )
+  }
+}
+
+ForEmployers = reduxForm({
+  form: 'employerContact',
+  fields,
+  validate
+})(ForEmployers)
+
+export default ForEmployers
