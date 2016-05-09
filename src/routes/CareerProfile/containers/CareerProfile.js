531b2a1bb30db298a69605af367c5b4035d7a881
Christian Ling <christian.s.h.ling@gmail.com>
2016-05-09 01:04:47 -0700

    Create CareerProfile container with hard-coded Science Librarian example

--- src/routes/CareerProfile/containers/CareerProfile.js
+++ src/routes/CareerProfile/containers/CareerProfile.js
@@ -3,6 +3,7 @@ import classes from './CareerProfile.scss'
 import { connect } from 'react-redux'
 
 import FontAwesome from 'react-fontawesome'
+import Slider from 'rc-slider'
 
 type Props = {
 
@@ -29,9 +30,9 @@ const careerProfileInformation = {
     'Write a blog post mentioning new publishing initiatives the library is undergoing and announce a new ' +
     'information source now available to the university that you selected that would be useful to all'],
   jobMetrics: {
-    salary: '',
-    collaboration: '',
-    hours: ''
+    salary: 60000,
+    collaboration: 2,
+    hours: 42
   },
   transferableSkills: ['Ability to converse with other researchers at a higher level of education and background',
     'Prior use of online databases specific to research (oe. PubMed), EndNote, BibTex',
@@ -45,6 +46,15 @@ const careerProfileInformation = {
 const userInformation = {
   originIndustry: 'Law',
   originJob: 'Lawyer'
+}
+
+const jobMetricsInformation = {
+  lowerSalaryBound: 40000,
+  upperSalaryBound: 160000,
+  lowerCollaborationBound: 0,
+  upperCollaborationBound: 10,
+  lowerHoursBound: 40,
+  upperHoursBound: 80
 }
 
 export class CareerProfile extends React.Component {
@@ -65,43 +75,115 @@ export class CareerProfile extends React.Component {
             && careerProfileInformation.faIcon
             && <FontAwesome className={classes.faIcon} name={careerProfileInformation.faIcon}/>}
         </div>
+
         <div className={classes.backButton}>Back</div>
         <div className={classes.title}>{careerProfileInformation.title}</div>
+
         <div className={classes.descriptionLabel}>Job Description</div>
-        <div className={classes.description}>
-          {careerProfileInformation.description.map(function(descriptionParagraph, index) {
-            return <p key={index}>{descriptionParagraph}</p>
-          })}
+        <div className={classes.informationContainer}>
+          <div className={classes.description}>
+            {careerProfileInformation.description.map(function(descriptionParagraph, index) {
+              return <p key={index}>{descriptionParagraph}</p>
+            })}
+          </div>
         </div>
+
         <div className={classes.exampleTasksLabel}>Examples of Tasks Performed on this Job</div>
-        <div className={classes.exampleTasks}>
-          <ul>
-            {careerProfileInformation.exampleTasks.map(function(exampleTask, index) {
-              return <li key={index}>{exampleTask}</li>
-            })}
-          </ul>
+        <div className={classes.informationContainer}>
+          <div className={classes.exampleTasks}>
+            <ul>
+              {careerProfileInformation.exampleTasks.map(function(exampleTask, index) {
+                return <li key={index}>{exampleTask}</li>
+              })}
+            </ul>
+          </div>
         </div>
+
         <div className={classes.jobMetricsLabel}>Job Metrics (relative to other potions)</div>
-        <div className={classes.jobMetrics}>
-          <div className={classes.salary}></div>
-          <div className={classes.collaboration}></div>
-          <div className={classes.hours}></div>
+        <div className={classes.informationContainer}>
+          <div className={classes.jobMetrics}>
+            <div className={classes.jobMetric}>
+              <div className={classes.salary}>
+                <div className={classes.jobMetricSliderLabel}>
+                  Salary
+                </div>
+                <Slider className={classes.jobMetricSlider}
+                        defaultValue={careerProfileInformation.jobMetrics.salary}
+                        disabled={true}
+                        included={true}
+                        max={jobMetricsInformation.upperSalaryBound}
+                        marks={{50000: 'Less', 150000: 'More'}}
+                        min={jobMetricsInformation.lowerSalaryBound}
+                        step={1000}
+                        tipFormatter={(salary) => {
+                        return 'The average annual salary for a ' + careerProfileInformation.title + ' is $' +
+                          salary + '.'
+                      }}
+                />
+              </div>
+            </div>
+            <div className={classes.jobMetric}>
+              <div className={classes.jobMetricSliderLabel}>
+                Collaboration
+              </div>
+              <div className={classes.collaboration}>
+                <Slider className={classes.jobMetricSlider}
+                        defaultValue={careerProfileInformation.jobMetrics.collaboration}
+                        disabled={true}
+                        included={true}
+                        max={jobMetricsInformation.upperCollaborationBound}
+                        marks={{1: 'Less', 9: 'More'}}
+                        min={jobMetricsInformation.lowerCollaborationBound}
+                        step={1}
+                        tipFormatter={(collaborationScore) => {
+                        return 'A ' + careerProfileInformation.title + ' typically works with ' + collaborationScore +
+                          ((collaborationScore === 1) ? ' colleague' : ' colleagues') + ' during a regular work day.'
+                      }}
+                />
+              </div>
+            </div>
+            <div className={classes.jobMetric}>
+              <div className={classes.jobMetricSliderLabel}>
+                Hours
+              </div>
+              <div className={classes.hours}>
+                <Slider className={classes.jobMetricSlider}
+                        defaultValue={careerProfileInformation.jobMetrics.hours}
+                        disabled={true}
+                        included={true}
+                        max={jobMetricsInformation.upperHoursBound}
+                        marks={{45: 'Less', 75: 'More'}}
+                        min={jobMetricsInformation.lowerHoursBound}
+                        step={1}
+                        tipFormatter={(hours) => {
+                        return 'On average, a ' + careerProfileInformation.title + ' works ' + hours + ' hours a week.'
+                      }}
+                />
+              </div>
+            </div>
+          </div>
         </div>
+
         <div className={classes.transferableSkillsLabel}>Transferable Skills from your Background in {userInformation.originIndustry}</div>
-        <div className={classes.transferableSkills}>
-          <ul>
-            {careerProfileInformation.transferableSkills.map(function(transferableSkill, index) {
-              return <li key={index}>{transferableSkill}</li>
-            })}
-          </ul>
+        <div className={classes.informationContainer}>
+          <div className={classes.transferableSkills}>
+            <ul>
+              {careerProfileInformation.transferableSkills.map(function(transferableSkill, index) {
+                return <li key={index}>{transferableSkill}</li>
+              })}
+            </ul>
+          </div>
         </div>
+
         <div className={classes.skillGapsLabel}>Skill Gaps coming from {userInformation.originIndustry}</div>
-        <div className={classes.skillGaps}>
-          <ul>
-            {careerProfileInformation.skillGaps.map(function(skillGap, index) {
-              return <li key={index}>{skillGap}</li>
-            })}
-          </ul>
+        <div className={classes.informationContainer}>
+          <div className={classes.skillGaps}>
+            <ul>
+              {careerProfileInformation.skillGaps.map(function(skillGap, index) {
+                return <li key={index}>{skillGap}</li>
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     )
