c3820678b7140a480816e4a0d043036a2ba63197
Christian Ling <christian.s.h.ling@gmail.com>
2016-05-09 19:15:53 -0700

    Create general ContactForm component

---
+++ src/forms/ContactForm/validateContactForm.js
@@ -0,0 +1,25 @@
+export default function validateContact(data, props) {
+  const errors = {};
+  if(!data.contactMessage) {
+    errors.contactMessage = 'Your message can\'t be empty!';
+  }
+
+  if(!data.contactName) {
+    errors.contactName = 'A contact name is required!';
+  }
+
+  if(!data.companyName) {
+    errors.companyName = 'A company name is required!';
+  }
+
+  if(!data.contactEmailAddress) {
+    errors.contactEmailAddress = 'A contact email address is required!';
+  }
+
+  if(!data.contactPhoneNumber) {
+    errors.phone = 'A contact phone number is required!';
+  } else if(!/\d{3}-\d{3}-\d{4}/.test(data.contactPhoneNumber)) {
+    errors.contactPhoneNumber = 'Phone must match the form "999-999-9999"'
+  }
+  return errors;
+}
