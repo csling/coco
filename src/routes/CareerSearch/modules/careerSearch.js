0ae993b609eb3540e202023dfbd4808d23e9b7c0
Christian Ling <christian.s.h.ling@gmail.com>
2016-05-09 22:07:58 -0700

    Code cleanup

--- src/routes/CareerSearch/modules/careerSearch.js
+++ src/routes/CareerSearch/modules/careerSearch.js
@@ -9,14 +9,6 @@ export const EMPLOYER_FORM_POST_FAILURE = 'EMPLOYER_FORM_POST_FAILURE'
 // Actions
 // ------------------------------------
 
-/*  This is a thunk, meaning it is a function that immediately
- returns a function for lazy evaluation. It is incredibly useful for
- creating async actions, especially when combined with redux-thunk!
-
- NOTE: This is solely for demonstration purposes. In a real application,
- you'd probably want to dispatch an action of COUNTER_DOUBLE and let the
- reducer take care of this logic.  */
-
 export function employerFormPostSuccess (): Action {
   console.log('success')
   return {
